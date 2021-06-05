c901bae1b9dbeaab579a78e70833e9f0939647f1
Sunil Patil <sdpatil87@gmail.com>
2021-06-06 00:46:20 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CourcesComponent } from './cources.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,7 +7,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourcesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cources.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'cources',
+    template:'<h2>Cources</h2>'
+})
+
+export class CourcesComponent{
+
+}
